9985bdfed93a7d5bb658c9f0fc658b3bb772e4b8
repl.it user <replituser@example.com>
2020-11-25 16:54:53 +0000

    Finish Ex 2d

--- likelihood.cc
+++ likelihood.cc
@@ -45,14 +45,42 @@ int main() {
     for (double mu = 0 ; mu < 6 ; mu += step_size) {
       foutlog  << mu << " " << -2 * log(prob(daten, mu)) << endl;
     }
-    fout.close();
+    foutlog.close();
+
+    // d)
+    ofstream foutdlog("deltanll.txt");
+    double lower_mu, upper_mu; // init confidence intervall variables
+    bool found_lower_mu = 0, found_upper_mu = 0; // set flags
+
+    double double_log_l_min = -2 * log(prob(daten, 3.11538));
+    for (double mu = 1 ; mu < 5 ; mu += step_size) {
+      foutdlog  << mu << " " << -2 * log(prob(daten, mu)) - double_log_l_min << endl;
+      double llratio = -2 * log(abs(prob(daten, mu)/ prob(daten, 3.11538)));
+      if (llratio < 1 && found_lower_mu == 0) {
+        lower_mu = mu;
+        found_lower_mu = 1;
+      }
+      if (llratio > 1 && found_lower_mu == 1 && found_upper_mu == 0) {
+        upper_mu = mu - step_size;
+        found_upper_mu = 1;
+      }
+
+    }
+    foutdlog.close();
+    cout << lower_mu << " < mu < " << upper_mu << endl;
+
 }
 
 // set terminal svg size 400,300 enhanced fname "arial", 10 butt solid
 // set output "likelihood.svg"
 // plot "likelihood.txt" using 1:2 smooth freq with boxes
 //
+// set terminal svg size 400,300 enhanced fname "arial", 10 butt solid
 // set output "nll.svg"
 // plot "nll.txt" using 1:2 smooth freq with lines
+
+// set terminal svg size 400,300 enhanced fname "arial", 10 butt solid
+// set output "deltanll.svg"
+// plot "deltanll.txt" using 1:2 smooth freq with lines
 
 // plot [2:4][0:10e-195] "likelihood.txt" using 1:2 smooth freq with line
